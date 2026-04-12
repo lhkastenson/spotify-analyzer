3d3cd7ddf016555905020a41e9611ac2df8e14db
Lon Kastenson <lon@Lons-MacBook-Air.local>
2026-04-11 23:27:57 -0400

    Add insert query

---
+++ src/spotify_analyzer/db.clj
@@ -0,0 +1,27 @@
+(ns spotify-analyzer.db
+  :require [next.jdbc :as jdbc]
+           [honey.sql :as sql]
+           [honey.sql.helpers :as h])
+
+(defn insert-play-events! [ds events]
+  (let [rows (map (fn [e]
+                    {:track_id     (:track-id e)
+                     :track_name   (:track-name e)
+                     :track_number (:track-number e)
+                     :duration_ms  (:duration-ms e)
+                     :popularity   (:popularity e)
+                     :artist_id    (:artist-id e)
+                     :artist_name  (:artist-name e)
+                     :album_type   (:album-type e)
+                     :album_tracks (:album-tracks e)
+                     :release_date (:release-date e)
+                     :played_at    (:played-at e)
+                     :context_type (:context-type e)
+                     :context_uri  (:context-uri e)})
+                  events)
+        query (-> (h/insert-into :play_events)
+                  (h/values rows)
+                  (h/on-conflict :played_at :track_id)
+                  (h/do-nothing)
+                  (sql/format))]
+    (jdbc/execute! ds query)))
